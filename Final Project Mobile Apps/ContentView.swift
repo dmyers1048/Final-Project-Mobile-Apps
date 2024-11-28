f22dea64f0ff1497057dc261a1dfa473d6b736e6
dmyers1048 <dmyers1@bsd220.org>
2024-11-28 11:49:18 -0600

    Initial Commit

---
+++ Final Project Mobile Apps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project Mobile Apps
+//
+//  Created by Devan Myers on 11/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final Project Mobile Apps/Final_Project_Mobile_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_Mobile_AppsApp.swift
+//  Final Project Mobile Apps
+//
+//  Created by Devan Myers on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_Mobile_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
